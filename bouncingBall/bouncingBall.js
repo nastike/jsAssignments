c44d880911ee75452801cf3f76591979b786359b
nastike <subheshjoshi@yahoo.com>
2023-10-17 14:37:48 +0545

    Bouncing Ball Initial Try

--- bouncingBall/bouncingBall.js
+++ bouncingBall/bouncingBall.js
@@ -4,11 +4,14 @@ let tags = document.getElementsByTagName('div');
 let smallBox = document.getElementById('small-box');
 let counter = 0;
 let topPosition = 10;
+
 smallBox.style.position = 'relative';
 setInterval(() => {
   if (topPosition < 450) {
     smallBox.style.top = topPosition + 'px';
+    smallBox.style.left = topPosition + 'px';
     topPosition += 5;
+    counter += 5;
   }
 }, 100);
 
