85320b64e618d5077085fee7c976d3fc8543faea
nastike <subheshjoshi@yahoo.com>
2023-10-17 08:56:21 +0545

    bouncing ball, made a ball

--- bouncingBall/bouncingBall.js
+++ bouncingBall/bouncingBall.js
@@ -1,1 +1,11 @@
 let smallBall = document.getElementById('container');
+let tags = document.getElementsByTagName('div');
+
+// console.log(tags);
+
+smallBall.style.background = 'green';
+let btn = document.getElementById('test-button');
+
+btn.onclick = () => {
+  alert('You clicked the button');
+};
