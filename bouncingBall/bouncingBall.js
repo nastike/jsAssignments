af7ee28bda05789e50b939749f1da625069b5c9b
nastike <subheshjoshi@yahoo.com>
2023-10-17 15:39:40 +0545

    Bouncing Ball final

--- bouncingBall/bouncingBall.js
+++ bouncingBall/bouncingBall.js
@@ -1,18 +1,25 @@
+// let container = document.getElementById('container');
+// let tags = document.getElementsByTagName('div');
+
 let smallBall = document.getElementById('container');
 let tags = document.getElementsByTagName('div');
 
 let smallBox = document.getElementById('small-box');
-let counter = 0;
 let topPosition = 10;
+let dy = 1;
 
 smallBox.style.position = 'relative';
 setInterval(() => {
-  if (topPosition < 450) {
-    smallBox.style.top = topPosition + 'px';
-    smallBox.style.left = topPosition + 'px';
-    topPosition += 5;
-    counter += 5;
+  if (topPosition > 450) {
+    dy = -1;
+  } else if (topPosition < 0) {
+    dy = 1;
   }
+
+  topPosition += dy * 5;
+
+  smallBox.style.top = topPosition + 'px';
+  smallBox.style.left = topPosition + 'px';
 }, 100);
 
 // console.log(tags);
