8a26b0840c1737d9266cc4b6e75651467bf1c1d8
nastike <subheshjoshi@yahoo.com>
2023-10-17 12:17:01 +0545

    ScatterPlot Final

--- scatterPlot/scatterPlot.js
+++ scatterPlot/scatterPlot.js
@@ -1,12 +1,22 @@
-let data = [
-  { top: 2, left: 5 },
-  { top: 0, left: 100 },
-  { top: 400, left: 0 },
-  { top: 1, left: 133 },
-  { top: 2, left: 100 },
-  { top: 5, left: 5 },
-  { top: 100, left: 2 },
-];
+// let data = [
+//   { top: 2, left: 5 },
+//   { top: 0, left: 100 },
+//   { top: 400, left: 0 },
+//   { top: 1, left: 133 },
+//   { top: 2, left: 100 },
+//   { top: 5, left: 5 },
+//   { top: 100, left: 2 },
+// ];
+let data = [];
+function generateDataForArray(numberOfData) {
+  for (let index = 0; index < numberOfData; index++) {
+    top = Math.floor(Math.random() * 200);
+    left = Math.floor(Math.random() * 200);
+    data.push({ top: top, left: left });
+  }
+}
+generateDataForArray(10);
+console.log(data);
 const container = document.getElementById('container');
 container.style.position = 'absolute';
 let idCounter = 1;
