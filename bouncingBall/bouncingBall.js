59029a369a53cdf1323a9f0c0d545c1b81751543
nastike <subheshjoshi@yahoo.com>
2023-11-21 10:29:17 +0545

    Rewrite Code

--- bouncingBall/bouncingBall.js
+++ bouncingBall/bouncingBall.js
@@ -1,33 +1,20 @@
-// let container = document.getElementById('container');
-// let tags = document.getElementsByTagName('div');
-
-let smallBall = document.getElementById('container');
-let tags = document.getElementsByTagName('div');
-let reset = document.getElementById('reset');
-
-let smallBox = document.getElementById('small-box');
+const container = document.getElementById('container');
+const bouncingBall = document.getElementById('small-box');
 let topPosition = 10;
-let sidePosition = 10;
+let leftPosition = 10;
 let dy = 10;
-let dx = 5;
-
-smallBox.style.position = 'relative';
+let dx = 10;
+bouncingBall.style.position = 'relative';
 setInterval(() => {
   if (topPosition > 450) {
     dy = -10;
     dx = -10;
-  } else if (topPosition < 0 && sidePosition < 0) {
+  } else if (topPosition < 0) {
     dy = 10;
     dx = 10;
   }
-
-  topPosition += dy * 2;
-  sidePosition += dx * 10;
-
-  smallBox.style.top = topPosition + 'px';
-  smallBox.style.left = sidePosition + 'px';
+  topPosition += dy;
+  leftPosition += dx;
+  bouncingBall.style.left = leftPosition + 'px';
+  bouncingBall.style.top = topPosition + 'px';
 }, 100);
-
-// console.log(tags);
-
-// smallBall.style.background = 'green';
