23d383d9d80702f0ec1fdfb1de237e1272d9a4ca
nastike <subheshjoshi@yahoo.com>
2023-11-22 08:04:10 +0545

    Rewrite Code

--- bouncingBall/bouncingBall.js
+++ bouncingBall/bouncingBall.js
@@ -10,23 +10,34 @@ let dx = 0;
 bouncingBall.style.position = 'absolute';
 secondBouncingBall.style.position = 'absolute';
 let positionArray = [
-  { top: 10, left: 10 },
-  { top: 10, left: 400 },
+  { top: 10, left: 10, dx: 0, dy: 10 },
+  { top: 450, left: 400, dx: 0, dy: 10 },
 ];
 
 setInterval(() => {
   if (positionArray[0].top > 450) {
-    dy = -10;
-    dx = 0;
+    positionArray[0].dy = -10;
+
+    console.log('greater than 450 condition');
   } else if (positionArray[0].top < 0) {
-    dy = 10;
-    dx = 0;
+    positionArray[0].dy = 10;
+
+    console.log('less than 0 condition');
+  }
+  if (positionArray[1].top > 450) {
+    positionArray[1].dy = -10;
+
+    console.log('greater than 450 condition');
+  } else if (positionArray[1].top < 0) {
+    positionArray[1].dy = 10;
+
+    console.log('less than 0 condition');
   }
 
-  positionArray[1].left += dx;
-  positionArray[1].top += dy;
-  positionArray[0].left += dx;
-  positionArray[0].top += dy;
+  positionArray[1].top += positionArray[1].dy;
+
+  positionArray[0].top += positionArray[0].dy;
+  console.log(positionArray[1].top);
   bouncingBall.style.left = positionArray[1].left + 'px';
   bouncingBall.style.top = positionArray[1].top + 'px';
   secondBouncingBall.style.left = positionArray[0].left + 'px';
